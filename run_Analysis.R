d0717f2bf847922e94260ec339ef481c21df64b6
arielaragon <cienciasariel@gmail.com>
2015-05-24 15:59:22 -0500

    Create run_Analysis.R

---
+++ run_Analysis.R
@@ -0,0 +1,9 @@
+##The variables describes the performance of the items and make a great job
+##with the next code have a simple analysis to know how is the relation and 
+##what is the mean, variance and standard deviation.
+todo.txt <- read.csv("~/Documents/Cursos/COURSERA/limpiadato/todo.txt.rtf")
+View(todo.txt)
+todo<-todo.txt
+mean(todo)
+var(todo)
+sd(todo)
