380015e7fddcd84945f44c95a318cd51c82e0a4b
arielaragon <cienciasariel@gmail.com>
2015-12-26 17:28:24 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,24 +1,34 @@
 library(shiny)
+library(shinythemes)
+shinyUI(fluidPage(theme=shinytheme("united"),
+                  headerPanel(
+                    HTML('Distributions of Random Variables v1: Showing some distribution type
+                         <a href="http://snap.uaf.edu" target="_blank"><img align="right" alt="SNAP Logo" src="./img/SNAP_acronym_100px.png" /></a>'
+                    ), "Distributions of Random Variables"
+                    ),
+                  fluidRow(
+                    column(4,
+                           wellPanel(
+                             radioButtons("dist","Distribution type:",
+                                          list("Normal"="norm","Uniform"="unif","t"="t","F"="F","Gamma"="gam","Exponential"="exp","Chi-square"="chisq","Log-normal"="lnorm","Beta"="beta")),
+                             sliderInput("n","Sample size:",1,1000,500),
+                             uiOutput("dist1"),
+                             uiOutput("dist2"),
+                             checkboxInput("density","Show density curve",FALSE),
+                             conditionalPanel(
+                               condition="input.density==true",
+                               numericInput("bw","bandwidth:",1)
+                             ),
+                             downloadButton("dldat", "Download Sample", class="btn-warning")
+                           )
+                    ),
+                    column(8,
+                           tabsetPanel(
+                             tabPanel("Plot",plotOutput("plot",height="600px")),
+                             tabPanel("Summary",verbatimTextOutput("summary")),
+                             tabPanel("Table",tableOutput("table"))
+                           )
+                    )
+                  )
+                  ))
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Hello Shiny!"),
-  
-  # Sidebar with a slider input for the number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-))
